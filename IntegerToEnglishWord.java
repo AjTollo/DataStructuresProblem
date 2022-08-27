05aaf1c9702ffaba30ab4a5558356a593d33cef9
Khesir <khesir2002@gmail.com>
2022-08-27 21:09:56 +0800

    Added new problems

--- IntegerToEnglishWord.java
+++ IntegerToEnglishWord.java
@@ -12,6 +12,7 @@ public class IntegerToEnglishWord {
 
             }
         };
+        System.out.println(tree);
     }
 
 }
---
+++ MissingNumber.java
@@ -0,0 +1,15 @@
+public class MissingNumber {
+    public static void main(String[] args) {
+
+    }
+
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = n * (n + 1) / 2;
+
+        for (int num : nums) {
+            sum -= num;
+        }
+        return sum;
+    }
+}
